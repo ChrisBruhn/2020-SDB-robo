1f8ae53d290c5065d6e5d4b2c60d63b3e687fa02
ToastMaster69 <mikk665k@elevcampus.dk>
2020-12-11 08:43:43 +0100

    My epic robots
    We got a SneakyPeaky and CrazySonOfABitch

---
+++ SneakyPeaky.java
@@ -0,0 +1,57 @@
+package prog;
+import robocode.*;
+
+// API help : https://robocode.sourceforge.io/docs/robocode/robocode/JuniorRobot.html
+
+/**
+ * SneakyPeaky - a robot by (your name here)
+ */
+public class SneakyPeaky extends JuniorRobot
+{
+	/**
+	 * run: SneakyPeaky's default behavior
+	 */
+	public void run() {
+		// Initialization of the robot should be put here
+
+		// Some color codes: blue, yellow, black, white, red, pink, brown, grey, orange...
+		// Sets these colors (robot parts): body, gun, radar, bullet, scan_arc
+		setColors(pink, pink, pink, pink, red);
+
+		// Robot main loop
+		while(true) {
+			// Replace the next 4 lines with any behavior you would like
+			ahead(100);
+			turnGunRight(360);
+			turnBodyLeft(90);
+			turnGunRight(360);
+			turnBodyRight(150);
+		}
+	}
+
+	/**
+	 * onScannedRobot: What to do when you see another robot
+	 */
+	public void onScannedRobot() {
+		// Replace the next line with any behavior you would like
+		fire(2);
+	}
+
+	/**
+	 * onHitByBullet: What to do when you're hit by a bullet
+	 */
+	public void onHitByBullet() {
+		// Replace the next line with any behavior you would like
+		back(10);
+		fire(1)
+	}
+	
+	/**
+	 * onHitWall: What to do when you hit a wall
+	 */
+	public void onHitWall() {
+		// Replace the next line with any behavior you would like
+		back(20);
+		turnBodyRight(90);
+	}	
+}
